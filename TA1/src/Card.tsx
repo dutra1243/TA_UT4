16ed73bcc70f2b53a6c2d61cdd7c8ddc09ba51ff
dutra1243 <facundodutraextra@gmail.com>
2024-09-25 19:52:39 -0300

    TA2

--- TA1/src/Card.tsx
+++ TA1/src/Card.tsx
@@ -1,17 +1,11 @@
 import React from 'react';
 import { ICardStuff } from './ICardStuff';
 
-export const Card = (cardStuff: ICardStuff) => {
+export const Card = ({ children }: { children: React.ReactNode }) => {
     return (
         <>
             <div className="cardStyle">
-
-                <p>{cardStuff.title}</p>
-                <p>{cardStuff.description}</p>
-                <p>{cardStuff.assignedTo}</p>
-                <p>{cardStuff.startDate}</p>
-                <p>{cardStuff.endDate}</p>
-                <br />
+                {children}
             </div>
         </>
     )
--- TA1/src/main.tsx
+++ TA1/src/main.tsx
@@ -7,9 +7,32 @@ import './index.css'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     {/* <App /> */}
-    <Card title="Card Title1" description="Card Description1" assignedTo="Assigned To1" startDate="Start Date1" endDate="End Date1" />
+    <Card >
+      <p> titulo 1</p>
+      <p>descripcion1</p>
+      <p>assignedTo1</p>
+      <p>startDate1</p>
+      <p>endDate</p>
+      <br />
+    </Card>
+    <Card >
+      <p> titulo 2</p>
+      <p>descripcion2</p>
+      <p>assignedTo2</p>
+      <p>startDate2</p>
+      <p>endDate2</p>
+      <br />
+    </Card>
+    <Card >
+      <p> titulo 3</p>
+      <p>descripcion3</p>
+      <p>assignedTo3</p>
+      <p>startDate3</p>
+      <p>endDate3</p>
+      <br />
+    </Card>
 
-    <Card title="Card Title2" description="Card Description2" assignedTo="Assigned To2" startDate="Start Date2" endDate="End Date2" />
-    <Card title="Card Title3" description="Card Description3" assignedTo="Assigned To3" startDate="Start Date3" endDate="End Date3" />
+    {/* <Card title="Card Title2" description="Card Description2" assignedTo="Assigned To2" startDate="Start Date2" endDate="End Date2" />
+    <Card title="Card Title3" description="Card Description3" assignedTo="Assigned To3" startDate="Start Date3" endDate="End Date3" /> */}
   </StrictMode>,
 )
