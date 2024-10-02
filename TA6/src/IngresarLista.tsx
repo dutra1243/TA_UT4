eb566d3595f6a90507fae8a87946fef03805cd70
Facundo Dutra Martin <facundodutraextra@gmail.com>
2024-10-02 20:04:33 -0300

    TA6 done

---
+++ TA6/src/IngresarLista.tsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react'
+
+import './App.css'
+
+
+export default function IngresarLista() {
+
+    const [tasks, setTask] = useState<string[]>(["task1"])
+
+
+    return (
+        <>
+            <input id="inputTask" type="text" placeholder="Ingrese la tarea" />
+            <button onClick={() => {
+                const inputElement = document.getElementById("inputTask") as HTMLInputElement;
+                if (inputElement.value !== "") {
+                    setTask([...tasks, inputElement.value]);
+                    inputElement.value = "";
+                }
+            }
+            }>Enviar</button>
+            <h1>Lista de tareas</h1>
+            <div>
+                <ul>
+                    {tasks.map((task) => (
+                        <li key={task}>{task}</li>
+                    ))}
+                </ul>
+            </div>
+        </>
+    )
+}
